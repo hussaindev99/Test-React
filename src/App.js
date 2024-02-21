cdf4483014069a39ad08abe5878467f3d0c1373c
hussaindev99 <naini.dev0@gmail.com>
2024-02-21 18:59:41 +0500

    Test React

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Sylani Web and Mobile-Batch-6</h1>
+      <h1>Assignment No.1</h1>
+      <h2>Learn With Naveed Sarwar</h2>
+      <h3>First Class</h3>
+      <h4>Batch-6</h4>
+      <h5>Let's start the journey</h5>
+      <h6>Let's learn and grow together</h6>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam veritatis molestias nulla dolor sit quas nostrum corrupti libero repudiandae. Est possimus voluptate autem ducimus facilis iure ipsam doloremque! Aliquam, magni?</p>
+    </div>
+  )
+}
+export default App;
